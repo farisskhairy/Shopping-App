e7e5e8512bd3ac1633fae80e47baa2c2c6ae4c4b
farisskhairy <khairyf@oregonstate.edu>
2025-04-24 14:30:31 -0700

    Added area for user to enter information in add page.

--- app/(tabs)/add_item_and_location_page.tsx
+++ app/(tabs)/add_item_and_location_page.tsx
@@ -1,11 +1,15 @@
 import { View, StyleSheet } from 'react-native';
 import Button_Add from '@/components/Button_Add';
+import Enter_Info_Add from '@/components/Enter_Info_Add';
 
 // Place to add item or store information.
 export default function Add_Item_and_Location_Page() {
     return (
         <View style={styles.container}>
-            <View style={styles.footerContainer}>
+            <View>
+                <Enter_Info_Add/>
+            </View>
+            <View>
                 <Button_Add/>
             </View>
         </View>
@@ -16,10 +20,6 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        padding: 6.5
-    },
-    footerContainer: {
-        flex: 1 / 3,
-        alignItems: 'center'
+        paddingTop: 6.5
     }
 });
