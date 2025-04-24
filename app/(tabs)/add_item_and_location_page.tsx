ab042deebea1e4873a1ca3bc31b79f7f58af0b36
farisskhairy <khairyf@oregonstate.edu>
2025-04-24 14:00:28 -0700

    Added add button to Add page.

--- app/(tabs)/add_item_and_location_page.tsx
+++ app/(tabs)/add_item_and_location_page.tsx
@@ -1,6 +1,25 @@
-import { } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import Button_Add from '@/components/Button_Add';
 
 // Place to add item or store information.
 export default function Add_Item_and_Location_Page() {
-    return;
+    return (
+        <View style={styles.container}>
+            <View style={styles.footerContainer}>
+                <Button_Add/>
+            </View>
+        </View>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        padding: 6.5
+    },
+    footerContainer: {
+        flex: 1 / 3,
+        alignItems: 'center'
+    }
+});
