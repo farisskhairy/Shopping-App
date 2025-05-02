b0c4ada48e9932bba0b8b52cef19b5f42e7a6b0b
farisskhairy <khairyf@oregonstate.edu>
2025-05-01 21:31:39 -0700

    Added server in server folder.

---
+++ server/server.js
@@ -0,0 +1,17 @@
+
+const express = require("express");
+
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hi.");
+});
+
+
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+});
